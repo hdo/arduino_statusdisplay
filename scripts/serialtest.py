272810934368d2b33c10bbdb9a904792c61e34f3
Huy Do <huy.do@gmx.de>
2014-10-07 16:47:33 +0200

    fixed typo

--- scripts/serialtest.py
+++ scripts/serialtest.py
@@ -3,7 +3,7 @@ import time
 
 print "Opening serial port ..."
 ser = serial.Serial('/dev/ttyUSB0', 38400, timeout=1)
-print "waiting 3 seconds ..."
+print "waiting 2 seconds ..."
 time.sleep(2)
 ser.write("\\01014B0082\n")
 print(ser.read(100))
