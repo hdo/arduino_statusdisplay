f9c2185179ef06a2221f14d2862e6a9271d83772
Huy Do <huy.do@gmx.de>
2014-10-07 10:08:24 +0200

    changed protocol to user readable data

--- Devices/ArduinoStatusdisplay/src/main.cpp
+++ Devices/ArduinoStatusdisplay/src/main.cpp
@@ -21,6 +21,10 @@ uint8_t processserial = 0;
 int ledPin = 13;    // LED connected to digital pin 13
 
 
+void update_leds() {
+    ws2812_setleds(led, LED_COUNT);
+}
+
 void init_leds() {
 	for(int i=0; i < LED_COUNT; i++) {
 		led[i].r=0;
@@ -40,25 +44,57 @@ void setup() {
 	pinMode(ledPin, OUTPUT);
 	Serial.begin(38400); // 0.2% error rate at 8 MHZ
 	init_leds();
-    ws2812_setleds(led, LED_COUNT);
+	update_leds();
+}
+
+int16_t parse_nibble(uint8_t nibble) {
+	if (nibble >= '0' && nibble <= '9') {
+		return nibble - '0';
+	}
+	if (nibble >= 'a' && nibble <= 'f') {
+		return (nibble - 'a') + 10;
+	}
+	if (nibble >= 'A' && nibble <= 'F') {
+		return (nibble - 'A') + 10;
+	}
+	return -1;
+}
+
+int16_t parse_hex_byte(uint8_t n1, uint8_t n0) {
+	int16_t parsed_n1 = parse_nibble(n1);
+	int16_t parsed_n0 = parse_nibble(n0);
+	if (parsed_n1 > -1 && parsed_n0 > -1) {
+		uint8_t ret = (0x0f & (uint8_t) parsed_n1) << 8;
+		ret |= (0x0f & (uint8_t) parsed_n0);
+		return ret;
+	}
+	return -1;
 }
 
 uint8_t parse_set_led_command() {
 	// 0: '\\'
 	// 1: '1' (set led command)
-	// 2: <channel> (0 - led count)
-	// 3: <value for R>
-	// 4: <value for G>
-	// 5: <value for B>
+	// 2-3: <channel as HEX> (0 - led count)
+	// 4-5: <value for R as HEX>
+	// 6-7: <value for G as HEX>
+	// 8-9: <value for B as HEX>
+	//
+	// example: \ 1 00 50 50 50
 
-	uint8_t index = serialdata[2];
-	if (serialcount >= 6) {
+	uint8_t led_index = parse_hex_byte(serialdata[2], serialdata[3]);
+	if (serialcount >= 10) {
 		// invalid command
-		if (index < LED_COUNT) {
-			cRGB value = {serialdata[3], serialdata[4], serialdata[5]};
-			set_led(index, value);
+		if (led_index > -1 && led_index < LED_COUNT) {
+			uint8_t value_r = parse_hex_byte(serialdata[4], serialdata[5]);
+			uint8_t value_g = parse_hex_byte(serialdata[6], serialdata[7]);
+			uint8_t value_b = parse_hex_byte(serialdata[8], serialdata[9]);
+
+			if (value_r > -1 && value_g > -1 && value_b > -1) {
+				cRGB value = {value_r, value_g, value_b};
+				set_led(led_index, value);
+				return 1;
+			}
 		}
-		return 1;
 	}
 	else {
 		// invalid command
@@ -109,7 +145,7 @@ int main(void) {
 			serialcount = 0;
 			processserial = 0;
 			if (ret) {
-				  ws2812_setleds(led, LED_COUNT);
+				update_leds();
 			}
 		}
 	}
